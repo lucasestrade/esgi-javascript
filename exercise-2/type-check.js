9a998e46f2699a7f8eeea0059631f8b0dd76d215
Lucas Estrade <lucas.estrade5@hotmail.fr>
2019-11-29 12:16:14 +0100

    ifhiof

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,6 +1,12 @@
 function type_check_v1(value, type) {
-    if (typeof type !== "string") return false;
-    return typeof value === type;
+    switch (typeof value) {
+        case "object":
+            if (Array.isArray(value)) return type === "array";
+            if (value === null) return null;
+            break;
+        default:
+            return value === type;
+    }
 }
 
 //console.log(type_check_v1(1, "number"));
@@ -9,27 +15,26 @@ function type_check_v2(value, object) {
     if (typeof value === "undefined" || value == null || value == "") return false;
     let theValue = null;
     let theReturn = false;
-    switch (typeof value) {
-        case "object":
-            let key = Object.keys(value)[0];
-            theValue = value[key];
-            break;
-        default:
-            theValue = value;
-    }
+    typeof value === "object" ? theValue = value[Object.keys(value)[0]] : theValue = value;
 
     if (Object.keys(object).includes("type")) {
         object.type === typeof theValue ? theReturn = true : theReturn = false;
-        if (Object.keys(object).includes("value")) {
-            object.value === theValue ? theReturn = true : theReturn = false;
-        }
+        if (!theReturn) return false;
     }
-
+    if (Object.keys(object).includes("value")) {
+        object.value === theValue ? theReturn = true : theReturn = false;
+        if (!theReturn) return false;
+    }
     if (Object.keys(object).includes("enum")) {
         object.enum.includes(value) ? theReturn = true : theReturn = false;
+        if (!theReturn) return false;
     }
 
     return theReturn;
 }
 
-//console.log(type_check_v2(1, { "type": "number", "enum": ["rgrg", 3, 2] }))
+//console.log(type_check_v2(1, { "type": "number", "enum": ["rgrg", 3, 1] }))
+
+function type_check(value, object) {
+
+}
